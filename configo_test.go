b661b3e86b4f5574f89fd8e7908929e5b35bbfcd
Shafreeck Sea <shafreeck@gmail.com>
2016-06-17 20:01:33 +0800

    Add Marshal test

--- configo_test.go
+++ configo_test.go
@@ -1,20 +1,31 @@
 package configo
 
 import (
+	//"github.com/naoina/toml"
+	"fmt"
 	"testing"
 )
 
 type Config struct {
 	Listen  string   `cfg:"listen, :8805, netaddr, listen address of server"`
 	Cluster []string `cfg:"cluster, ['127.0.0.1:8800'], dialstring"`
+	MaxConn int      `cfg:"max-conn, required, dialstring"`
 }
 
 func TestUnmarshal(t *testing.T) {
 	//data := []byte("listen = ':8804'")
-	data := []byte("")
+	data := []byte("max-conn=100")
 	c := &Config{}
 	if err := Unmarshal(data, c); err != nil {
 		t.Error(err)
 	}
 	t.Logf("Unmarshal result %s\v\n", c)
+}
+func TestMarshal(t *testing.T) {
+	c := &Config{}
+	if data, err := Marshal(c); err != nil {
+		t.Error(err)
+	} else {
+		fmt.Printf("%s", data)
+	}
 }
