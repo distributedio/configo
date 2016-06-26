385ce42bfa8a7d9bc00f90860b2ccb8214289e2f
Shafreeck Sea <shafreeck@gmail.com>
2016-06-26 22:10:48 +0800

    Add util configo-build to build a conf generator

---
+++ bin/configo-build/main.go
@@ -0,0 +1,83 @@
+//Use configo-build to build a conf generator
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
+)
+
+var source = `package main
+import(
+	"flag"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/shafreeck/configo"
+
+	"%s" // the config package
+)
+func main() {
+   base := ""
+   flag.StringVar(&base, "patch", "", "the base conf that to patch")
+   flag.Parse()
+
+    config := %s{} //the struct
+
+    if base != "" {
+        baseData, err := ioutil.ReadFile(base)
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+        if data, err := configo.Patch(baseData, &config); err != nil {
+            fmt.Println(err)
+            return
+        } else {
+            fmt.Print(string(data))
+        }
+        return
+    }
+
+    if data, err := configo.Marshal(config); err != nil {
+        fmt.Println(err)
+        return
+    } else {
+        fmt.Printf(string(data))
+    }
+}
+`
+
+func main() {
+	args := os.Args
+	if len(args) != 2 {
+		fmt.Println("Usage:")
+		fmt.Printf("  %s <package>.<struct>\n", args[0])
+		return
+	}
+
+	pkg := args[1]
+	idx := strings.LastIndex(pkg, ".")
+	if idx < 0 {
+		log.Fatalln("Invalid package format, expected <package>.<struct name>")
+	}
+	p := pkg[0:idx]
+	st := path.Base(p) + "." + pkg[idx+1:]
+	code := fmt.Sprintf(source, p, st)
+	//fmt.Print(code)
+
+	tmpFile := fmt.Sprintf("/tmp/%s.pkg.go", st)
+	if err := ioutil.WriteFile(tmpFile, []byte(code), os.ModePerm&0660); err != nil {
+		log.Fatalln(err)
+	}
+
+	out := strings.ToLower(st) + ".cfg"
+	cmd := exec.Command("go", "build", "-o", out, tmpFile)
+	if err := cmd.Run(); err != nil {
+		log.Fatalln(err)
+	}
+}
