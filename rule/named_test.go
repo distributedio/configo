2cce0e8f93e6fc98a9e6bd74f3c63fb3ac7714cd
Shafreeck Sea <shafreeck@gmail.com>
2016-07-10 19:12:19 +0800

    Add more unittests

---
+++ rule/named_test.go
@@ -0,0 +1,28 @@
+package rule
+
+import (
+	"testing"
+)
+
+func TestValidatePath(t *testing.T) {
+	if err := ValidatePath("/abc"); err != nil {
+		t.Fatal(err)
+	}
+	if err := ValidatePath("./abc"); err != nil {
+		t.Fatal(err)
+	}
+	if err := ValidatePath("../abc"); err != nil {
+		t.Fatal(err)
+	}
+	if err := ValidatePath(".../abc"); err == nil {
+		t.Fatal()
+	} else {
+		t.Log(err)
+	}
+	if err := ValidatePath("./../abc"); err != nil {
+		t.Fatal(err)
+	}
+	if err := ValidatePath(".././../abc"); err != nil {
+		t.Fatal(err)
+	}
+}
